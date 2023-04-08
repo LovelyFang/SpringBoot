5e8e5a946958cb6440cabf88804d935a35052764
liu_df <liu_df@haici.com>
2023-04-08 13:20:57 +0800

    稀里哗啦

--- SpringBootExampleTest/src/main/java/com/tsxy/controller/Health.java
+++ SpringBootExampleTest/src/main/java/com/tsxy/controller/Health.java
@@ -1,11 +1,20 @@
 package com.tsxy.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.tsxy.utils.PropertyUtil;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author Liu_df
@@ -13,7 +22,9 @@ import java.util.Properties;
  */
 
 @RestController
-public class Health {
+public class Health implements InitializingBean {
+
+    private ThreadPoolTaskExecutor executorService = null;
 
     @RequestMapping("/health")
     public String health() {
@@ -26,4 +37,73 @@ public class Health {
         return property;
     }
 
+    @RequestMapping("/getCountDownLatch")
+    public String getCountDownLatch() throws Exception {
+        CountDownLatch latch = new CountDownLatch(4);
+        List<String> result = new ArrayList<>();
+        System.out.println(LocalTime.now() + "  开始执行");
+        executorService.execute(() -> {
+            try {
+                result.add("abc");
+                System.out.println(LocalTime.now() + " 增加abc");
+            } catch (Exception e){
+                System.out.println("查询处方订单异常");
+            } finally {
+                latch.countDown();
+            }
+        });
+        System.out.println(LocalTime.now() + " ==> " + JSON.toJSONString(result));
+        executorService.execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                result.add("123");
+                System.out.println(LocalTime.now() + " 增加123");
+            } catch (Exception e){
+                System.out.println("查询处方订单异常");
+            } finally {
+                latch.countDown();
+            }
+        });
+        System.out.println(LocalTime.now() + " ==> " + JSON.toJSONString(result));
+        executorService.execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                result.add("abc123");
+                System.out.println(LocalTime.now() + " 增加abc123");
+            } catch (Exception e){
+                System.out.println("查询处方订单异常");
+            } finally {
+                latch.countDown();
+            }
+        });
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(LocalTime.now() + " ==> " + JSON.toJSONString(result));
+        executorService.execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                result.add("123abc");
+                System.out.println(LocalTime.now() + " 增加123abc");
+            } catch (Exception e){
+                System.out.println("查询处方订单异常");
+            } finally {
+                latch.countDown();
+            }
+        });
+        System.out.println(LocalTime.now() + " ==> " + JSON.toJSONString(result));
+        latch.await();
+        System.out.println(LocalTime.now() + "  处理完成");
+        return JSON.toJSONString(result);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.executorService = new ThreadPoolTaskExecutor();
+        this.executorService.setCorePoolSize(20);
+        this.executorService.setMaxPoolSize(1000);
+        this.executorService.setThreadNamePrefix("queryOrder-");
+        this.executorService.setQueueCapacity(800);
+        this.executorService.setKeepAliveSeconds(120);
+        this.executorService.setAllowCoreThreadTimeOut(true);
+        this.executorService.initialize();
+    }
 }
